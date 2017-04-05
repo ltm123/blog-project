d8be6764a582a59a0ca9757285c7a9111fa80675
ltm123 <woaidyue@163.com>
2017-04-05 21:58:36 +0800

    add admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from models import *
+
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'click_count',)  # 显示项
+    list_display_links = ('title', 'desc',)  # 链接项 不能与可编辑项相同
+    list_editable = ('click_count',)  # 可编辑项
+
+    # 添加时，分类显示各项
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content', 'user', 'category', 'tag',)
+        }),
+        ('高级设置', {
+            'classes': ('collapse',),
+            'fields': ('click_count', 'is_recommend',)
+        }),
+    )
+
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
